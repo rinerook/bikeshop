3e87b7e240c4367504e92c96fb59051b3c5df404
Karina Hochstein <karina.hochstein@gmx.de>
2017-01-14 17:17:01 +0100

    4.3 - Controllers - added Orders

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    # @test_variable = 42 # DELETE LATER
   end
 
   # GET /products/1
