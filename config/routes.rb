008308091c77d60666a6e0d584724476ef9c1464
Karina Hochstein <karina.hochstein@gmx.de>
2017-01-19 16:54:04 +0100

    5.1 - routes, links in navigation works now

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products 			# cause we generated our products controller via scaffolding, it added this route as wel
   get 'static_pages/about'
 
   get 'static_pages/contact'
-
+  
   # get 'static_pages/index'
   # root 'static_pages#index'
   root 'static_pages#landing_page'
+  # root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :orders, only: [:index, :show, :create, :destroy]
