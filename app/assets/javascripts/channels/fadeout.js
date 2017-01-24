378521e27cda86ba000824ea2051b08aa21a9559
karinahochstein <karina.hochstein@gmx.de>
2017-01-24 16:43:54 +0100

    added js fadeout

---
+++ app/assets/javascripts/channels/fadeout.js
@@ -0,0 +1,7 @@
+$(document).on('turbolinks:load', function(){
+   	$(".alert").delay(2000).fadeOut("slow");
+ });
+
+/* $( document ).on('turbolinks:load', function() {
+  console.log("It works on each visit!")
+})*/
