617a30abececab862b3be1c35a86f298be25ce83
Karina Hochstein <karina.hochstein@gmx.de>
2017-02-01 11:57:50 +0100

    Task 6.2 again, fixed product spec, added specs for models

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,19 +1,19 @@
-require 'rails_helper'		# to get access to configuration needed to write rspec tests
+require 'rails_helper'    # to get access to configuration needed to write rspec tests
 
-describe Product do
-  before do
-    @product = Product.create!(name: "race bike", description: "test trallalala", image_url: "9.jpg", colour: "hallo", price: "199")
+describe Product do       # tell rspec we want to test product model
+  before do               # create product, user and ratings  
+    @product = Product.create!(name: "race bike", description: "test", image_url: "9.jpg", colour: "hallo", price: "199")
     @user = User.create!(email: "localhorst@gmx.de", password: "password")
     @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
-    @product.comments.create!(rating: 3, user: @user, body: "Nice bike!")
-    @product.comments.create!(rating: 5, user: @user, body: "Great bike!")
+    @product.comments.create!(rating: 2, user: @user, body: "Nice bike!")
+    @product.comments.create!(rating: 3, user: @user, body: "Great bike!")
   end
 
-  it "returns the average rating of all comments" do
-      
+  it "returns the average rating of all comments" do  #despricption of what we want to happen
+    expect(@product.average_rating).to eq 2  
   end
 
-  it "it not valid" do
+  it "is not valid" do
     expect(Product.new(description: "Nice bike")).not_to be_valid
   end
 
