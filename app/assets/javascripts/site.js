19d767dd513692dab7b6f33baa1999df2a9e1aa6
Karina Hochstein <karina.hochstein@gmx.de>
2017-02-07 10:50:02 +0100

    6.9 - Javascript in rails, added jquery elevateZoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,12 @@
-$(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+$(document).on('turbolinks:load', function(){	// Turbolinks speed up the page load time 
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets',
+		readOnly: true,
+		score: function() {
+			return $(this).attr('data-score');
+		}
+	});
+	$('.img-zoom').elevateZoom({ zoomType: "inner", 
+		cursor: "crosshair"
+	});
 });
