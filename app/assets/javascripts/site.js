b12b9e7793a913d6e2a6b6f94204af93d891cf00
karinahochstein <karina.hochstein@gmx.de>
2017-02-07 18:14:55 +0100

    6.10 - AJAX, leave comment and rating without reloading page

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,16 @@
+var refreshRating = function() {
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+  	readOnly: true,
+  	score: function() {
+  		return $(this).attr('data-score');
+  	}
+  });
+};
+
 $(document).on('turbolinks:load', function(){	// Turbolinks speed up the page load time 
-	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets',
-		readOnly: true,
-		score: function() {
-			return $(this).attr('data-score');
-		}
-	});
-	$('.img-zoom').elevateZoom({ zoomType: "inner", 
+  refreshRating();
+  $('.img-zoom').elevateZoom({ zoomType: "inner", 
 		cursor: "crosshair"
 	});
 });
