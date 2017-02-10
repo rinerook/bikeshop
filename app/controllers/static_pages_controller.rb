3dd65c1a3442229403c5dc3a92e8e67ecbbd0693
Karina Hochstein <karina.hochstein@gmx.de>
2017-02-10 09:19:44 +0100

    Contact form validation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
 
   def landing_page
   	@featured_product = Product.first
-  	@products = Product.limit(3)
+  	@products = Product.limit(5)
   end
 
   def thank_you
