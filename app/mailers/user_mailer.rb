2f0e8f009aea5c5ccb010a1021400dd19cecc607
Karina Hochstein <karina.hochstein@gmx.de>
2017-02-10 10:14:27 +0100

    User welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,4 +8,11 @@ class UserMailer < ApplicationMailer
 	      :to => 'rine@nurfuerspam.de',
 	      :subject => "A new contact form message from #{name}")
 	end
+
+	def welcome(user)
+	  @appname = "Bike Shop"
+	  mail( :to => user.email,
+	        :subject => "Welcome to #{@appname}!")
+	end
+	
 end
