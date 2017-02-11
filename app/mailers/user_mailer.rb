77d1b7907db4e876275fd556b286f5c287dfbc39
karinahochstein <karina.hochstein@gmx.de>
2017-02-11 11:45:22 +0100

    user mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+
+	default from: "rine@nurfuerspam.de"
+
+	def contact_form(email, name, message)
+	@message = message
+	  mail(:from => email,
+	      :to => 'rine@nurfuerspam.de',
+	      :subject => "A new contact form message from #{name}")
+	end
+end
